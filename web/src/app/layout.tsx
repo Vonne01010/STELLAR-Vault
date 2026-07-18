138e19b057e97d55d55618049e6006265af612be
Vonne01010 <sumbeling.vcr@gmail.com>
2026-07-18 23:21:50 +0800

    feat: toast notification added to new folder structure

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ToastProvider } from "@/components/Toast";
+import { ToastProvider } from "@/components/shared/Toast";
 import "./globals.css";
 
 const geistSans = Geist({
